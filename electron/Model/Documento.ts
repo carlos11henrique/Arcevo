d138dabce6281f3fe06863a86e8a6a357d8aa12b
carlos11henrique <henrique.lima2679@gmail.com>
2025-08-27 20:40:30 -0300

    correcao index

--- electron/Model/Documento.ts
+++ electron/Model/Documento.ts
@@ -1,17 +1,37 @@
-import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm";
-import { Documento } from "./Documento";
+import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, OneToMany } from "typeorm";
+import { Categoria } from "./Categoria";
+import { Local } from "./Local";
+import { Movimentacao } from "./Movimentacao";
 
 @Entity()
-export class Categoria {
+export class Documento {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Column()
+  titulo: string;
+
   @Column({ unique: true })
-  nome: string;
+  codigo: string;
 
   @Column({ nullable: true })
   descricao: string;
 
-  @OneToMany(() => Documento, documento => documento.categoria)
-  documentos: Documento[];
+  @Column({ type: "date", nullable: true })
+  dataCriacao: Date;
+
+  @Column({ type: "timestamp", default: () => "CURRENT_TIMESTAMP" })
+  dataRegistro: Date;
+
+  @Column({ default: "ativo" })
+  status: "ativo" | "arquivado" | "emprestado" | "perdido";
+
+  @ManyToOne(() => Categoria, categoria => categoria.documentos)
+  categoria: Categoria;
+
+  @ManyToOne(() => Local, local => local.documentos)
+  local: Local;
+
+  @OneToMany(() => Movimentacao, movimentacao => movimentacao.documento)
+  movimentacoes: Movimentacao[];
 }
